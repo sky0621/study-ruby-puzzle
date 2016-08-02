4b9f4197152e05ed45c2638f632b70b9ead1c089
Shotaro Sasaki <fspia.biz@gmail.com>
2016-08-02 23:51:48 +0900

    q002 continue

--- q002/a002_01.rb
+++ q002/a002_01.rb
@@ -9,13 +9,40 @@ class A002
 		p "Answer"
 		(1000..1234).each do |i|
 			p i
-			if isReverse(i)
-
-			end
+#			if isReverse(i)
+p targets(i)
+#			end
 p " "
 p " "
 p " "
 		end
+	end
+
+	def targets(num)
+		targets = []
+		tempArray = []
+		# f.e. 1234 -> [1][2][3][4]
+		numArray = num.to_s.split("")
+		# 1 -> 2 -> 3 -> 4
+		numArray.each do |snum|
+			if targets.empty?
+				@@OP.each do |op|
+					targets << snum + op
+				end
+				# now : targets = [1*][1/][1+][1-]
+			else
+				# f.e. [1*] -> [1/] -> [1+] -> [1-]
+				targets.each do |target|
+					@@OP.each do |op|
+						tempArray << target + snum + op
+					end
+					# now : tempArray = [1*2*][1*2/][1*2+][1*2-][1/2*][1/2/][1/2+][1/2-][1+2*][1+2/][1+2+][1+2-][1-2*][1-2/][1-2+][1-2-]
+				end
+				targets = tempArray
+				tempArray = []
+			end
+		end
+		return targets
 	end
 
 	def isReverse(num)
