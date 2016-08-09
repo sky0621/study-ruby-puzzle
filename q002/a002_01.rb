f31dcf032842d1f60ab1ddd546628b3b39a50de0
Shotaro Sasaki <fspia.biz@gmail.com>
2016-08-10 06:12:18 +0900

    q002 fin

--- q002/a002_01.rb
+++ q002/a002_01.rb
@@ -4,11 +4,13 @@ class A002
 
 	public 
 
-	@@OP = %w[* / + -]
+#	@@OP = %w[* / + -]
+	@@OP = %w[* /]
 
 	def answer
 		p "Answer"
 		(1000..9999).each do |num|
+			# 1数ずつの計算パターン
 			nums = num.to_s.split("")
 			@@OP.each do |op|
 				@@OP.each do |op2|
@@ -20,8 +22,14 @@ class A002
 						if nums[1] != "0"
 							culc += (op + nums[1])
 						end
+						if eval(culc).to_s == num.to_s.reverse
+							pp num
+						end
 						if nums[2] != "0"
 							culc += (op2 + nums[2])
+						end
+						if eval(culc).to_s == num.to_s.reverse
+							pp num
 						end
 						if nums[3] != "0"
 							culc += (op3 + nums[3])
@@ -29,6 +37,59 @@ class A002
 						if eval(culc).to_s == num.to_s.reverse
 							pp num
 						end
+					end
+				end
+			end
+			# 2数ずつの計算パターン
+			pre = nums[0].to_s
+			post  = (nums[1] + nums[2] + nums[3]).to_i.to_s
+			@@OP.each do |op|
+				if post == "0"
+					next
+				end
+				if eval(pre + op + post).to_s == num.to_s.reverse
+					pp num
+				end
+			end
+			# 3数の計算パターン1
+			first = nums[0].to_s
+			second = nums[1].to_s
+			third = (nums[2] + nums[3]).to_i.to_s
+			@@OP.each do |op|
+				@@OP.each do |op2|
+					if second == "0" || third == "0"
+						next
+					end
+					if eval(first + op + second + op2 + third).to_s == num.to_s.reverse
+						pp num
+					end
+				end
+			end
+			# 3数の計算パターン2
+			first = nums[0].to_s
+			second = (nums[1] + nums[2]).to_i.to_s
+			third = nums[3].to_s
+			@@OP.each do |op|
+				@@OP.each do |op2|
+					if second == "0" || third == "0"
+						next
+					end
+					if eval(first + op + second + op2 + third).to_s == num.to_s.reverse
+						pp num
+					end
+				end
+			end
+			# 3数の計算パターン3
+			first = (nums[0] + nums[1]).to_i.to_s
+			second =nums[2].to_s
+			third = nums[3].to_s
+			@@OP.each do |op|
+				@@OP.each do |op2|
+					if second == "0" || third == "0"
+						next
+					end
+					if eval(first + op + second + op2 + third).to_s == num.to_s.reverse
+						pp num
 					end
 				end
 			end
